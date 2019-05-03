719f67bc31a78f77deb7f80a05645eff6a828339
isalevine <isaacdlevine@gmail.com>
2019-05-03 16:30:35 -0700

    tutorial complete, starting work on ActionCable

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import Canvas01 from './components/Canvas01'
+
+function App() {
+  return (
+    <div className="App">
+      <Canvas01 />
+    </div>
+  );
+}
+
+export default App;
